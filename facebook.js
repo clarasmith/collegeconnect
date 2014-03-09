761ec806dfe3f343ae74e3616279d74524bb612d
Megan Chen <mchen0000@gmail.com>
2014-03-09 12:16:36 -0400

    fb

--- facebook.js
+++ facebook.js
@@ -17,7 +17,8 @@ window.fbAsyncInit = function() {
       // login status of the person. In this case, we're handling the situation where they 
       // have logged in to the app.
       testAPI();
-	  //window.location.href = "./study.html";
+	  if(location.pathname == "/signin.html")
+	  	window.location.href = "./study.html";
     } else if (response.status === 'not_authorized') {
       // In this case, the person is logged into Facebook, but not into the app, so we call
       // FB.login() to prompt them to do so. 
@@ -54,4 +55,5 @@ window.fbAsyncInit = function() {
     FB.api('/me', function(response) {
       console.log('Good to see you, ' + response.name + '.');
     });
+	
   }
